c83c38993507aa7408bcd3ae7bac323a8ad69139
Arup3201 <arupjana7365@gmail.com>
2025-09-01 22:27:10 +0530

    fix: variable route syntax error

--- backend/routes/projects.py
+++ backend/routes/projects.py
@@ -25,7 +25,8 @@ def create_project():
     })
 
 def get_project(project_id: str):
-    project_details = ProjectService().get_project(project_id)
+    user_payload = User(**request.environ["user"])
+    project_details = ProjectService().get_project(project_id=project_id, user_id=user_payload.id)
     return jsonify({
         "message": f"project {project_id} fetched", 
         "data": project_details
@@ -57,14 +58,14 @@ def assign_task(project_id: str, task_id: str):
 
 projects_blueprint.add_url_rule("/", endpoint="list-projects", view_func=list_projects, methods=["GET"])
 projects_blueprint.add_url_rule("/", endpoint="create-project", view_func=create_project, methods=["POST"])
-projects_blueprint.add_url_rule("/{project_id}", endpoint="get-project", view_func=get_project, methods=["GET"])
-projects_blueprint.add_url_rule("/{project_id}", endpoint="delete-project", view_func=delete_project, methods=["DELETE"])
-projects_blueprint.add_url_rule("/{project_id}/members", endpoint="get-project-members", view_func=get_members, methods=["GET"])
+projects_blueprint.add_url_rule("/<project_id>", endpoint="get-project", view_func=get_project, methods=["GET"])
+projects_blueprint.add_url_rule("/<project_id>", endpoint="delete-project", view_func=delete_project, methods=["DELETE"])
+projects_blueprint.add_url_rule("/<project_id>/members", endpoint="get-project-members", view_func=get_members, methods=["GET"])
 
-projects_blueprint.add_url_rule("/join/code/{project_code}", endpoint="join-project", view_func=join_project, methods=["POST"])
+projects_blueprint.add_url_rule("/join/code/<project_code>", endpoint="join-project", view_func=join_project, methods=["POST"])
 
-projects_blueprint.add_url_rule("/{project_id}/tasks", endpoint="create-project-task", view_func=create_task, methods=["POST"])
-projects_blueprint.add_url_rule("/{project_id}/tasks/{task_id}", endpoint="get-task", view_func=get_task, methods=["GET"])
-projects_blueprint.add_url_rule("/{project_id}/tasks/{task_id}", endpoint="edit-project-task", view_func=edit_task, methods=["PUT"])
-projects_blueprint.add_url_rule("/{project_id}/tasks/{task_id}/status", endpoint="change-project-task-status", view_func=change_task_status, methods=["PUT"])
-projects_blueprint.add_url_rule("/{project_id}/tasks/{task_id}/assign", endpoint="assign-project-task", view_func=assign_task, methods=["PUT"])
+projects_blueprint.add_url_rule("/<project_id>/tasks", endpoint="create-project-task", view_func=create_task, methods=["POST"])
+projects_blueprint.add_url_rule("/<project_id>/tasks/<task_id>", endpoint="get-task", view_func=get_task, methods=["GET"])
+projects_blueprint.add_url_rule("/<project_id>/tasks/<task_id>", endpoint="edit-project-task", view_func=edit_task, methods=["PUT"])
+projects_blueprint.add_url_rule("/<project_id>/tasks/<task_id>/status", endpoint="change-project-task-status", view_func=change_task_status, methods=["PUT"])
+projects_blueprint.add_url_rule("/<project_id>/tasks/<task_id>/assign", endpoint="assign-project-task", view_func=assign_task, methods=["PUT"])
