a08da420ce8d5ec2d5f9aa0b75f00a8aa5899e02
Arup3201 <arupjana7365@gmail.com>
2025-09-01 11:26:20 +0530

    feat: project page without api added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router"
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import Dashboard from "./pages/Dashboard"
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Project from "./pages/Project";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route index element={<Login />}/>
-        <Route path="/register" element={<Register />}/>
+        <Route index element={<Login />} />
+        <Route path="/register" element={<Register />} />
 
-        <Route path="projects" element={<Dashboard />} />
+        <Route path="/projects" element={<Dashboard />} />
+        <Route path="/projects/:id" element={<Project />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
